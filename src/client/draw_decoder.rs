3d76c1291eff2f68bd2d780a19734220f3186a73
Antoine Martin <totaam@xpra.org>
2025-04-10 22:57:47 +0700

    specify we want packed RGB, log size

--- src/client/draw_decoder.rs
+++ src/client/draw_decoder.rs
@@ -1,6 +1,7 @@
 use log::{info, trace};
 
-// use zune_core;
+use zune_core::colorspace::ColorSpace;
+use zune_core::options::DecoderOptions;
 use zune_jpeg;
 use zune_png;
 
@@ -9,9 +10,14 @@ pub fn decode(coding: &String, data: Vec<u8>) -> Result<Vec<u8>, String>{
     info!("decode {:?}: {:?} bytes", coding, data.len());
     trace!("data={:?}", data);
     if coding == "jpeg" {
-        let mut decoder = zune_jpeg::JpegDecoder::new(&data);
+        let options = DecoderOptions::default().jpeg_set_out_colorspace(ColorSpace::RGB);
+        let mut decoder = zune_jpeg::JpegDecoder::new_with_options(&data, options);
         match decoder.decode() {
-            Ok(data) => return Ok(data),
+            Ok(data) => {
+                let info = decoder.info().unwrap();
+                info!("size: {:?}x{:?}", info.width, info.height);
+                return Ok(data);
+            },
             Err(e) => return Err(format!("jpeg decoding error: {:?}", e))
         };
     }
